c859b0a84874e84144c3aedb5d813e3650904d86
Dynamic Body Therapy <dynamicbt@forge.dreamhost.com>
2012-06-09 19:51:45 -0700

    robots

--- wp-content/themes/overeasy/footer.php
+++ wp-content/themes/overeasy/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
   <?php 
-    print"Dynamic Body Therapy, Suite 109, Level One, The Block Arcade, 280 Collins St, Melbourne, VIC 3000 ph 03 9670 0443"; ?>
+    print"Dynamic Body Therapy, Suite 109, Level One, The Block Arcade, 280 Collins St, Melbourne, VIC 3000 ph (03) 9650 0360"; ?>
 <a href="/wp-admin>staff</a>
 </div>
